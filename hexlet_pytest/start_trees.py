20afa99aa16f6e8204467abb289678a52210e23a
seeu359 <a.ch3re359@yandex.ru>
2022-07-11 16:24:56 +0300

    Started new course on Hexlet - 'Trees'. Added first exercise and test for it.

---
+++ hexlet_pytest/start_trees.py
@@ -0,0 +1,14 @@
+def remove_first_level(tree):
+    list1 = []
+    lst2 =[]
+    for i in tree:
+        if type(i) == list:
+            list1.append(i)
+    for it in list1:
+        for item in it:
+            lst2.append(item)
+    return lst2
+
+
+a = [1, 100, 3]
+print(remove_first_level(a))
---
+++ tests/test_start_trees.py
@@ -0,0 +1,14 @@
+from hexlet_pytest.start_trees import remove_first_level
+
+def test_remove_first_level():
+    assert remove_first_level([]) == []
+    assert remove_first_level([1, 100, 3]) == []
+    assert remove_first_level([1, 2, [3, 5], [[4, 3], 2]]) == [3, 5, [4, 3], 2]
+    assert remove_first_level([[5], 1, [3, 4]]) == [5, 3, 4]
+    assert remove_first_level([
+        [1, [3, 2]],
+        2,
+        [3, 5],
+        2,
+        [130, [1, [550, 10]]],
+    ]) == [1, [3, 2], 3, 5, 130, [1, [550, 10]]]
